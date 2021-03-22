657bca57567b0820146fb90938ab0c75c6a99a75
amparcabezas <ampacabezas@gmail.com>
2021-03-22 14:12:49 +0100

    inicio

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Footer from '@/components/Footer'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Footer
   }
 }
 </script>
