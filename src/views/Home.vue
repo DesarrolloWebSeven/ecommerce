60a23abd4875dda3e83580669264d0d0e6e885db
amparcabezas <ampacabezas@gmail.com>
2021-03-22 15:12:00 +0100

    icono

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="https://aspergerasturias.org/wp-content/uploads/2016/03/Logo-AAA_horizontal2_smll1.jpg">
+    
   </div>
 </template>
 
