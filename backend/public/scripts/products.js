567b80f6ddd5bf862b82302b0a9642da9142858c
Accountant77 <vbonilla.77@hotmail.com>
2021-04-07 21:53:55 +0200

    terminando la funcion update

--- backend/public/scripts/products.js
+++ backend/public/scripts/products.js
@@ -1,5 +1,3 @@
-console.log("Hola")
-
 window.onload=()=>{
     showProducts()
 }
--- backend/routers/rtAdmin.js
+++ backend/routers/rtAdmin.js
@@ -8,6 +8,7 @@ rtAdmin.get('/productos', adminController.products_index)
 rtAdmin.post('/productos', adminController.products_save)
 rtAdmin.get('/productos/listar', adminController.products_list)
 rtAdmin.delete('/productos/eliminar/:id', adminController.products_delete)
+rtAdmin.get('/productos/actualizar/:id', adminController.products_findById)
 rtAdmin.put('/productos/actualizar/:id', adminController.products_update)
     
 module.exports= rtAdmin
