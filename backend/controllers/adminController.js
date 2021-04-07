051e3811b56c8fdbd1c713cca954c19d848f94f3
Accountant77 <vbonilla.77@hotmail.com>
2021-04-07 13:16:05 +0200

    add delete products and update

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -9,10 +9,10 @@ const products_index = (req,res)=>{
 }
 
 const products_save = (req,res)=>{
-    console.log(req.files)
+    //console.log(req.files)
     let images=[]
     req.files.forEach(i=>images.push('/images/'+i.filename))
-    console.log(images)
+    //console.log(images)
     req.body.images = images
     let product = new Product(req.body)
     product.save()
@@ -25,19 +25,29 @@ const products_save = (req,res)=>{
 const products_list = (req,res)=>{
         Product.find().then(data=>res.json(data))        
 }
+const products_delete = async (req,res)=>{
+    console.log(req.params.id)
+    const product_delete = await Product.findByIdAndDelete(req.params.id)
+    res.send("Articulo Eliminado")
+    console.log("Se ha borrado: " + product_delete)
+}
+
+const products_update = async (req,res)=>{
+    console.log(req.params.id)
+    Product.findById(req.params.id)
+    .then(product=>{
+        console.log(product)
+        res.render('products', {product:product})
+    })
+    //const product_update = await Product.findByIdAndUpdate()
+}
 
 
 module.exports = {
     login,
     products_index,
     products_save,
-    products_list
+    products_list,
+    products_delete,
+    products_update
 }
-
-
-
-
-
-
-
-
--- backend/routers/rtAdmin.js
+++ backend/routers/rtAdmin.js
@@ -7,5 +7,7 @@ rtAdmin.get('/', adminController.login)
 rtAdmin.get('/productos', adminController.products_index)
 rtAdmin.post('/productos', adminController.products_save)
 rtAdmin.get('/productos/listar', adminController.products_list)
+rtAdmin.delete('/productos/eliminar/:id', adminController.products_delete)
+rtAdmin.put('/productos/actualizar/:id', adminController.products_update)
     
 module.exports= rtAdmin
