7505273116856b270b08566ed7881675c82d610e
Accountant77 <vbonilla.77@hotmail.com>
2021-04-13 13:58:47 +0200

    unificando el public

--- backend/helpers/upload.js
+++ backend/helpers/upload.js
@@ -2,14 +2,14 @@ const multer = require('multer')
 const path = require('path')
 
 const storage = multer.diskStorage({
-    destination: path.join(__dirname, '../public/images'),
+    destination: path.join(__dirname, '../../public/images'),
     filename:(req, file, cb)=>{
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     }
 })
 const upload = {
     storage,
-    dest: path.join(__dirname, '../public/images'),
+    dest: path.join(__dirname, '../../public/images'),
     limits: {fieldSize:10000000},
     fileFilter: (req, file, cb)=>{
         const fileTypes = /jpeg|jpg|png|gif|svg|/ 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const multer = require('multer')
+const path = require('path')
 const app = express()
 
 // Database settings
@@ -20,7 +21,7 @@ app.use(multer(upload).array('images'))
 
 // Middlewares
 app.use(cors())
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(path.join(__dirname, '../public')))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
