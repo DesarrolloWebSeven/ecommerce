544d983da20754b02a6e9f8dfd25aebe19357697
amparcabezas <ampacabezas@gmail.com>
2021-04-13 22:07:02 +0200

    style navbar y home

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,42 +1,46 @@
 <template>
+  <div class="container">
+      
+      <div class="row">
+          <div v-for="product in products" :key="product.index" class="col-12 col-md-6">
+      <Product :product="product" />
+    </div>
 
-<div v-for="product in products" :key="product.index">
-    <Product :product="product" />
-</div>
-  
+      </div>
+    
+  </div>
 </template>
 
 <script>
-import Product from '@/components/Product'
-import {reactive} from 'vue'
+import Product from "@/components/Product";
+import { reactive } from "vue";
 export default {
-    name:'Category',
-    components:{
-        Product
-    },
-    props:{
-        category:String
-    },
-    setup(props){
+  name: "Category",
+  components: {
+    Product,
+  },
+  props: {
+    category: String,
+  },
+  setup(props) {
+    let products = reactive([]);
+    const listProduct = () => {
+      fetch(`http://localhost:8081/productos/${props.category}`)
+        .then((res) => res.json())
+        .then((data) => data.forEach((item) => products.push(item)))
+        .catch((err) => console.log(err));
+    };
+    listProduct();
 
-        let products=reactive([])
-        const listProduct=()=>{
-            fetch(`http://localhost:8081/productos/${props.category}`)
-            .then(res=>res.json())
-            .then(data=>data.forEach(item=>products.push(item)))
-            .catch(err=>console.log(err))
-        }
-        listProduct()        
-        
-        return{
-            products
-        }
-    }
-
-
-}
+    return {
+      products,
+    };
+  },
+};
 </script>
 
-<style lang="sass" scoped>
-
+<style lang="scss" scoped>
+.container{
+    margin-top: 20px;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,88 @@
 <template>
   <div class="container section">
-    <div clas="row">
-        <img class="col-12 icono" src="@/assets/imgHome.png">
+    <div class="row">
+      <h1 class="titleHome">Regalos para informaticxs</h1>
+      
+      <router-link to="/productos/camisetas" class="col-6 category">
+        <div class="inner">Camisetas <div><img class="logoGeeky" src="../../public/images/images-1618226795740.png" alt=""> <img class="logoGeeky" src="../../public/images/images-1618226285755.png" alt=""></div> </div>
+      </router-link>
+
+      <router-link to="/productos/accesorios" class="col-6 category">
+        <div class="inner">Accesorios <div><img class="logoGeeky" src="../../public/images/images-1618228602474.png" alt=""> <img class="logoGeeky" src="../../public/images/images-1618227834601.png" alt=""></div> </div>
+      </router-link>
+
+      <router-link to="/productos/tazas" class="col-6 category">
+        <div class="inner">Tazas <div><img class="logoGeeky" src="../../public/images/images-1617973995105.png" alt=""> <img class="logoGeeky" src="../../public/images/images-1618225574181.png" alt=""></div></div>
+      </router-link>
+
+      <router-link to="/productos/games" class="col-6 category">
+        <div class="inner">Juegos <div><img class="logoGeeky" src="../../public/images/images-1618230322660.jpg" alt=""><img class="logoGeeky" src="../../public/images/images-1618229687036.jpg" alt=""></div></div>
+      </router-link>
+      
     </div>
-    
+
+
     
   </div>
 </template>
 
 <script>
-import { useStore } from 'vuex'
-import { computed } from 'vue'
+import { useStore } from "vuex";
+import { computed } from "vue";
 export default {
-  name: 'Home',
-  setup(){
-
+  name: "Home",
+  setup() {
     return {
-      lang: computed(()=>useStore().getters.getLang)
-    }
+      lang: computed(() => useStore().getters.getLang),
+    };
   },
-  components: {
-    
-    
-  }
-}
+  components: {},
+};
 </script>
 <style lang="scss" scoped>
-.section{
+.section {  
+  background-color: #10555e1e;
   max-width: 80%;
- 
-  
-  
-   
+  margin-top: 30px;
+  color: black;
+  .row{
+    max-width: 95%;
+    margin:0 auto;
+  }
+  .category{
+    text-transform: uppercase;
+    color:white;
+    padding: 0 5px 0 5px;
+      
+  }
+  .category .inner {
+    border-left: 8px solid black;
+    border-bottom: 8px solid black;
+    color: black;
+    padding: 10px;
+    margin-bottom: 20px;
+    min-height: 100px;
+    font-size: 30px;
+    font-family: "Montserrat" !important;
+    font-weight: bold;
+    
 }
-
+  .titleHome{
+    font-family:"Game" !important;
+    font-size:50px;
+    background-color: #0f606b;
+    padding: 30px;
+    border-top: 8px solid black;
+    border-bottom: 8px solid black;
+    margin-bottom: 30px;
+    margin-top: 30px;
+  }
+  img{
+    width: 200px;
+    height: 200px;
+    background-color: white;
+    margin: 5px;
+  }
+  
+}
 </style>
