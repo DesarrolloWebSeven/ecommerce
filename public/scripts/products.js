c7b5376e911fd7945ce6de60661a967fa44d4458
Accountant77 <vbonilla.77@hotmail.com>
2021-04-19 21:38:35 +0200

    .

--- public/scripts/products.js
+++ public/scripts/products.js
@@ -55,7 +55,7 @@ function showProducts(){
                         method:'DELETE',
                         headers: {'Content-type':'Application/json'}
                     })
-                    showProducts()
+                    showProducts()//Aqui termine
                 }
             }
         })
