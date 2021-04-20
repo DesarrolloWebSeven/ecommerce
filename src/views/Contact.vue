56f6c0f2afd2889a16e93796119a36e78c2149b0
Nuria Martínez <nuria.martinezro@gmail.com>
2021-04-20 15:44:48 +0200

    Formulario de contacto

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,98 +1,149 @@
 <template>
-  <div class="section">
-    <h5>{{lang["descriptionContactP1"]}}</h5>
-    <form class="row g-3">
-      <div class="col-md-6">
-        <label class="form-label">{{lang["name"]}}</label>
+  <main class="container">
+    <section class="row section">
+      <h1 class="text-center">{{ lang["descriptionContactP1"] }}</h1>
+      <form class="col-12 col-lg-6 mb-md-3" @submit.prevent="contactMail">
+        <div class="row">
+        <div class="col-6">
+        <label class="form-label">{{ lang["name"] }}</label>
         <input
           type="text"
-          name="firstName"
+          v-model="firstName"
           id="firsName"
+          class="form-control"
+          :placeholder="lang['plName']"
+        />
+        </div>
+        <div class="col-6">
+        <label class="form-label">{{ lang["lastName"] }}</label>
+        <input
+          type="text"
+          v-model="lastName"
+          id="lastName"
           class="form-control"
           :placeholder="lang['plLastName']"
         />
-      </div>
-      <div class="col-md-6">
-        <label class="form-label">{{lang["lastName"]}}</label>
-        <input
-          type="text"
-          name="lastName"
-          id="lastName"
-          class="form-control"
-         :placeholder="lang['plName']"
-        />
-      </div>
-      <div class="col-md-6">
-        <label class="form-label">{{lang["email"]}}</label>
+        </div>
+        </div>
+        <label class="form-label">{{ lang["email"] }}</label>
         <input
           type="email"
-          name="email"
+          v-model="email"
           id="email"
           class="form-control"
           :placeholder="lang['plEmail']"
         />
-      </div>
-
-      <div class="col-md-6">
-        <label class="form-label">{{lang["type"]}}</label>
-        <select type="consult" name="consult" id="consult" class="form-select">
-          <option selected>{{lang["select1"]}}</option>
-          <option>{{lang["select2"]}}</option>
-          <option>{{lang["select3"]}}</option>
-          <option>{{lang["select4"]}}</option>
+        <label class="form-label">{{ lang["type"] }}</label>
+        <select v-model="subject" id="consult" class="form-select">
+          <option selected>{{ lang["select1"] }}</option>
+          <option>{{ lang["select2"] }}</option>
+          <option>{{ lang["select3"] }}</option>
+          <option>{{ lang["select4"] }}</option>
         </select>
+        <label class="form-label">{{ lang["descriptionQuery"] }}</label>
+        <textarea class="form-control" v-model="message" id="texto" rows="3"></textarea>
+        <div class="alert alert-success" role="alert" v-if="success">{{ success }}</div>
+        <button class="d-flex flex-row btn btn-primary">
+          {{ lang["sendForm"] }}
+        </button>
+      </form>
+      <div class="d-sm-none d-lg-block col-lg-6">
+        <iframe
+          class="map"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3040.789771417989!2d-3.6987739852400594!3d40.34700936780193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4226dc6a64c46b%3A0x23e1b239d4aea935!2sLa%20Nave!5e0!3m2!1ses!2ses!4v1618921704011!5m2!1ses!2ses"
+          style="border: 0"
+          allowfullscreen=""
+          loading="lazy"
+        ></iframe>
       </div>
-      <div class="col">
-  <label class="form-label">{{lang["descriptionQuery"]}}</label>
-  <textarea class="form-control" id="texto" rows="3"></textarea>
-</div>
-
-      <div class="col-12 d-flex justify-content-center">
-        <button type="submit" class="btn btn-primary">{{lang["sendForm"]}}</button>
-      </div>
-    </form>
-    <div class="col-md-6">
-      <h3>Nos encontramos en Madrid....</h3> 
-    </div>
-    
-  </div>
+    </section>
+  </main>
 </template>
 
 <script>
-import { useStore } from 'vuex'
-import { computed } from 'vue'
+import axios from 'axios'
+import { useStore } from "vuex";
+import { computed, ref } from "vue";
 export default {
   name: "HelpContact",
-  setup(){
+  setup() {
+    let firstName = ref("")
+    let lastName = ref("")
+    let email = ref("")
+    let subject = ref("")
+    let message = ref("")
+    let success = ref("")
+
+    const contactMail = async () => {
+
+      try {
+
+        const res = await axios.post('usuario/contacto', {
+          firstName: firstName.value,
+          lastName: lastName.value,
+          email: email.value,
+          subject: subject.value,
+          message: message.value
+        })
+        if (res.status === 200) success.value = "Su mensaje se ha enviado con éxito"
+        
+      } catch (err) {
+        console.log(err.message)
+      }
+
+    }
+
 
     return {
-      lang: computed(()=>useStore().getters.getLang)
-    }
-  }
-  
+      success,
+      firstName,
+      lastName,
+      email,
+      subject,
+      message,
+      contactMail,
+      lang: computed(() => useStore().getters.getLang),
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.section{
-  max-width: 80%;
-  margin: 0 auto;
-  margin-top:20px;
-  padding:20px;
-  background-color: #fff;
+section {
+  color: black;
+  margin: 40px 0;
+  padding: 0 50px 0;
+}
+
+h1 {
+  margin-bottom: 40px;
+}
+
+label {
+  display: block;
+  color: black;
+  margin-bottom: 3px;
   text-align: left;
-  color:black;
-  border-radius:25px;  
-  h5{
-    margin-bottom: 20px;
-  }
-  button{
-    background-color: #0F606B;
-  }
-  button:hover{
+}
+
+input, select, textarea, .success {
+  margin-bottom: 10px;
+}
+
+button {
+  border: none;
+  justify-content: center;
+  width: 40%;
+  background-color: #0f606b;
+}
+
+button:hover {
   background-color: #707070;
-  } 
-  
-  
+}
+
+.map {
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
 }
 </style>
