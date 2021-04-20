e339a799fcd7656fc525df30197263362ab3830b
Accountant77 <vbonilla.77@hotmail.com>
2021-04-20 12:42:37 +0200

    solventando conflictos

--- backend/routers/rtAdmin.js
+++ backend/routers/rtAdmin.js
@@ -2,7 +2,6 @@ const express = require('express')
 const rtAdmin = express.Router()
 const adminController = require('../controllers/adminController')
 const productController = require('../controllers/productController')
-const { isAdmin, passLocal } = require('../helpers/auth')
 const { checkUser, requireAuth } = require('../helpers/validation')
 
 
@@ -23,12 +22,12 @@ rtAdmin.get('/clientes', requireAuth, adminController.clients)
 rtAdmin.get('/pedidos', requireAuth, adminController.orders)
 
 // Product routes
-rtAdmin.get('/productos', isAdmin, productController.productsIndex)
-rtAdmin.post('/productos', isAdmin, productController.productsSave)
-rtAdmin.get('/productos/listar', isAdmin, productController.productsList)
-rtAdmin.delete('/productos/:id', isAdmin, productController.productsDelete)
-rtAdmin.get('/productos/:id', isAdmin, productController.productsFindById)
-rtAdmin.delete('/productos/imagenes/:id', isAdmin, productController.imagesDelete)
-rtAdmin.post('/productos/update', isAdmin, productController.productsUpdate)
+rtAdmin.get('/productos', requireAuth, productController.productsIndex)
+rtAdmin.post('/productos', requireAuth, productController.productsSave)
+rtAdmin.get('/productos/listar', requireAuth, productController.productsList)
+rtAdmin.delete('/productos/:id', requireAuth, productController.productsDelete)
+rtAdmin.get('/productos/:id', requireAuth, productController.productsFindById)
+rtAdmin.delete('/productos/imagenes/:id', requireAuth, productController.imagesDelete)
+rtAdmin.post('/productos/update', requireAuth, productController.productsUpdate)
     
 module.exports= rtAdmin
