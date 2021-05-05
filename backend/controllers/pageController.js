132c18c5dfd5c6d377b65d3484168495ce737162
Accountant77 <vbonilla.77@hotmail.com>
2021-05-05 12:23:24 +0200

    .

--- backend/controllers/pageController.js
+++ backend/controllers/pageController.js
@@ -4,26 +4,31 @@ const Page = require('../models/Page')
 const pageUpdate = (req, res) => {
   req.body.title = req.params.title
   Page.find({ title: req.body.title }).lean()
-    .then(data => updateContent(data))
+    .then(data => {
+      updateContent(data)
+        .then(page => {
+          res.render('project', ({ title: "Admin | Proyecto", css: 'project', src:'staticPages.js' }))
+        })
+      .catch(err => console.log(err.message))
+    })
     .catch(err => res.json(err))
 }
 
 // Search and update the content
 const updateContent = (data) => {
-  Page.findByIdAndUpdate({_id : data[0]._id}, {
-    title : data[0].title,
-    content : data[0].content,
-    images : data[0].images,
-  },
-  function(err, result) {
-      if (err) console.log('No se pudo actualizar: ' + err) 
-      else console.log('Se actualizo: ' + result)
-  }).lean()
-    .then(page => {
-      console.log('Producto actualizado correctamente: ' + page)
-      res.render('project', ({ title: "Admin | Proyecto", css: 'project', src:'staticPages.js' }))
-    })
-    .catch(err => console.log(err.message))
+  return new Promise((reject, resolved)=>{
+    Page.findByIdAndUpdate({_id : data[0]._id}, {
+      title : data[0].title,
+      content : data[0].content,
+      images : data[0].images,
+    },
+    function(err, result) {
+        if (err) console.log('No se pudo actualizar: ' + err) 
+        else console.log('Se actualizo: ' + result)
+    }).lean()
+    .then(data=>resolved(data))
+    .catch(error=>reject(error))
+  })
 }
 
 // Show content in the Front End
