83dbb6b32a60d687f7b3c6abb82dc3554793fba7
amparcabezas <ampacabezas@gmail.com>
2021-05-15 16:41:16 +0200

    ok

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -5,7 +5,7 @@ describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
-      props: { msgh }
+      props: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
   })
